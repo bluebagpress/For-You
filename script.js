fd235ba454f780893d2b723e04717fd3f360fc61
bluebagpress <bluebagpress@gmail.com>
2026-06-30 21:18:03 -0400

    script.js

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ const total = 40;
 
 function showPage() {
 
-    const number = String(current).padStart(2, "0");
+    alert(current);
 
+    const number = String(current).padStart(2, "0");
     page.src = `${number}.jpg`;
 
 }
